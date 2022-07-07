b0faa449e3bac93bd71968fc5aaf4f66ddc3e401
ving-developer <henrique.sb0709@gmail.com>
2022-07-07 01:46:27 -0300

    feat: Adding LatestMessagesActivity
    -Creating LatestMessagesActivity.kt
    -Calling LatestMessagesActivity.kt when register in RegisterActivity.kt
    -Change launch activity to LatestMessagesActivity

---
+++ app/src/main/java/com/example/envoyapp/LatestMessagesActivity.kt
@@ -0,0 +1,44 @@
+package com.example.envoyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+
+class LatestMessagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latest_messages)
+        verifyUserLoggedIn()
+    }
+
+    private fun verifyUserLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if(uid == null){
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_new_message -> {
+
+            }
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/com/example/envoyapp/RegisterActivity.kt
+++ app/src/main/java/com/example/envoyapp/RegisterActivity.kt
@@ -92,6 +92,9 @@ class RegisterActivity : AppCompatActivity() {
         ref.setValue(user)
             .addOnSuccessListener {
                 Log.d("RegisterActivity", "User has been saved in Firebase Database")
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
             }
             .addOnFailureListener {
                 Log.d("RegisterActivity", "Failed to save user in Database: ${it.message}")
