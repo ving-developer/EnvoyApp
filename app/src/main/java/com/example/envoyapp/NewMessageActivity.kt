ccf6a088b2efcac741deac09ca6bee1515fdcbe3
ving-developer <henrique.sb0709@gmail.com>
2022-07-07 05:22:56 -0300

    feat: creating NewMessageActivity.kt
    -Including com.squareup.picasso:picasso:2.8 to manage images
    -Including "com.github.lisawray.groupie:groupie:2.10.1" and "com.github.lisawray.groupie:groupie-viewbinding:2.10.1" to manage recycler view
    -Creating  user_row_new_manage to manage layout of lines

---
+++ app/src/main/java/com/example/envoyapp/NewMessageActivity.kt
@@ -0,0 +1,69 @@
+package com.example.envoyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.envoyapp.databinding.ActivityNewMessageBinding
+import com.example.envoyapp.databinding.UserRowNewMessageBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieAdapter
+import com.xwray.groupie.viewbinding.BindableItem
+
+class NewMessageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewMessageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewMessageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Select User"
+
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val adapter = GroupieAdapter()
+                snapshot.children.forEach {
+                    Log.d("NewMessageActivity",it.toString())
+                    val user = it.getValue(User::class.java)
+                    if(user != null){
+                        adapter.add(UserItem(user))
+                    }
+                }
+                binding.recyclerviewNewmessage.adapter = adapter
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+}
+
+class UserItem(val user: User) : BindableItem<UserRowNewMessageBinding>() {
+    override fun bind(viewBinding: UserRowNewMessageBinding, position: Int) {
+        viewBinding.usernameTextviewNewmessage.text = user.username
+        Picasso.get().load(user.profileImageUrl).into( viewBinding.imageviewNewmessage)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+
+    override fun initializeViewBinding(view: View): UserRowNewMessageBinding {
+        return UserRowNewMessageBinding.bind(view)
+    }
+
+
+}
--- app/src/main/java/com/example/envoyapp/RegisterActivity.kt
+++ app/src/main/java/com/example/envoyapp/RegisterActivity.kt
@@ -105,4 +105,6 @@ class RegisterActivity : AppCompatActivity() {
     }
 }
 
-class User(val uid: String, val username: String, val profileImageUrl: String)
+class User(val uid: String, val username: String, val profileImageUrl: String) {
+    constructor(): this("","","")
+}
